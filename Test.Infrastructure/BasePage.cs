d7504fd9232dc7f53706c69645072f9ceb74ce68
ilan.adasha <ilana@codevalue.com>
2024-12-05 18:35:02 +0200

    adding chrome driver

---
+++ Test.Infrastructure/BasePage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Test.Infrastructure
+{
+    internal abstract class BasePage
+    {
+        public readonly IWebDriver driver;
+        public readonly WebDriverWait wait;
+
+        public BasePage(IWebDriver _driver)
+        {
+            driver = _driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+
+    }
+}
